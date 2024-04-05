ecd454957e0edd3e62afe7b171f4389d76eab9b2
Roger Santos <rsantosth@gmail.com>
2024-04-05 01:06:51 -0300

    adding routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,13 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.get("/hello", (req, res) => res.send("Hello!"));
+
+app.post("/webhook", (req, res) => res.status(200).json({
+    status: 200,
+    success: true
+}));
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
